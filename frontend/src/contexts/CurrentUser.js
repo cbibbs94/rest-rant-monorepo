5ae4938339ab149a849898bb0b8c33b2af1e6f0d
cbibbs94 <89940267+cbibbs94@users.noreply.github.com>
2022-04-19 00:14:41 -0700

    re wrote the code from the excersise so i actually know whats going on

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { useEffect } from 'react';
 
 
 export const CurrentUser = createContext()
@@ -6,7 +7,15 @@ export const CurrentUser = createContext()
 function CurrentUserProvider({ children }){
 
     const [currentUser, setCurrentUser] = useState(null)
-
+    useEffect(() => {
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:5000/authentication/profile')
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
+    
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
             {children}
